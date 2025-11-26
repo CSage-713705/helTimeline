2f65b50ae76c0529a63fef88fc3cfa32645bfaef
Sage <3365174583@qq.com>
2025-11-26 15:49:48 +0800

    英文主标题副标题优化

--- src/App.jsx
+++ src/App.jsx
@@ -31,14 +31,17 @@ export default function App() {
           >
             <div className="flex flex-col items-center">
               <h1 className="font-serif text-4xl md:text-5xl font-normal text-white leading-tight mb-6">
-                英国近现代文学"叙事权威"演变分析
+                Timeline of English Literature
               </h1>
+              <h2 className="font-serif text-2xl md:text-3xl font-normal text-white leading-tight mb-6">
+                Narrative Authority and Human Interpretation of Novels
+              </h2>
               <p className="text-gray-100 font-sans max-w-xl px-2 text-lg leading-relaxed mb-6 justify">
                 This timeline showcases 5 representative works from the Age of Enlightenment to the contemporary era.<br/>
                 <br/>
-                You can see the characteristics and construction of narrative authority of each work, as well as what interpretation of human nature they represent.
+                You can see the characteristics and construction of narrative authority of each work, as well as what interpretation and exploration of human nature they represent.
                 {/* 本时间轴展示了由启蒙时代到当代5个关键文学时期的代表作品。
-                您可以阅览各作品叙事权威的特征与构建方式，以及它们代表了何种对人性的诠释。 */}
+                您可以阅览各作品叙事权威的特征与构建方式，以及它们代表了何种对人性的诠释与追求。 */}
                 {/* TODO:添加中文翻译版本 */}
 
               </p>
